0a74dabf5bc6c2541c327aae0f4cd1919d640c4c
Carolina perez <mariacarola46@hotmail.com>
2022-06-24 10:33:33 -0300

    odificaciones en los estilos

--- src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-iniciar-sesion',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iniciar-sesion.component.css']
 })
 export class IniciarSesionComponent implements OnInit {
+form:FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { 
+    this.form = this.formBuilder.group(
+      {
+        nombre:['', Validators.required],
+        contraseña:['', Validators.required]
+
+    }
+    )
+  }
 
   ngOnInit(): void {
   }
+
+  get Nombre(){return this.form.get('nombre')}
+  get Contraseña(){return this.form.get('contraseña')}
 
 }
