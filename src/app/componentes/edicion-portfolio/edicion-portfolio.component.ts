b716da72d0c4e165819bbe8d989d5e8e9885768a
Carolina perez <mariacarola46@hotmail.com>
2022-06-25 01:14:18 -0300

    Creacion de componente para la edicion del postfolio

---
+++ src/app/componentes/edicion-portfolio/edicion-portfolio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edicion-portfolio',
+  templateUrl: './edicion-portfolio.component.html',
+  styleUrls: ['./edicion-portfolio.component.css']
+})
+export class EdicionPortfolioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
