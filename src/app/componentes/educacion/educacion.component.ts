46d8d396e8af1a3658c4b3fc1ac8d2d0dd187cf7
Carolina perez <mariacarola46@hotmail.com>
2022-06-26 02:54:06 -0300

    Se agrego los enlaces para editar, eliminar y agregar elementos en el componente educacion

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Educacion } from 'src/app/modelo/educacion';
 
 @Component({
   selector: 'app-educacion',
@@ -7,7 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducacionComponent implements OnInit {
 
-  constructor() { }
+  esEdicion: boolean = false;
+
+  educacion: Educacion[] = [
+    {
+      id: 1,
+      nombreInstitucion: "Nombre Instituto",
+      tituloObtenido: "Carrera o titulo obtenido",
+      logoInstitucional: "./assets/educacion.png",
+      desde: "03/2018",
+      hasta: "Actualidad",
+    },
+    {
+      id: 1,
+      nombreInstitucion: "Nombre Instituto",
+      tituloObtenido: "Carrera o titulo obtenido",
+      logoInstitucional: "./assets/educacion.png",
+      desde: "03/2018",
+      hasta: "05/2021",
+    },
+    {
+      id: 1,
+      nombreInstitucion: "Nombre Instituto",
+      tituloObtenido: "Carrera o titulo obtenido",
+      logoInstitucional: "./assets/educacion.png",
+      desde: "03/2018",
+      hasta: "Actualidad",
+    }
+  ]
+
+
+  constructor(private router: Router) {
+    if (this.router.url === '/edicionPortfolio')
+      this.esEdicion = true;
+  }
 
   ngOnInit(): void {
   }
