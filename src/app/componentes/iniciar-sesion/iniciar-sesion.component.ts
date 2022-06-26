2252fc3e5d52e24d1d7a8c330caed5eb6a3942d4
Carolina perez <mariacarola46@hotmail.com>
2022-06-26 02:56:29 -0300

    Se agrego componente para el inicio de sesion

--- src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/componentes/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,28 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from 'src/app/servicios/auth.service';
+
 @Component({
   selector: 'app-iniciar-sesion',
   templateUrl: './iniciar-sesion.component.html',
   styleUrls: ['./iniciar-sesion.component.css']
 })
 export class IniciarSesionComponent implements OnInit {
-form:FormGroup;
+  form: FormGroup;
 
-  constructor(private formBuilder:FormBuilder) { 
+  private nombreUsuario: string = '';
+  private contraseña: string = '';
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
     this.form = this.formBuilder.group(
       {
-        nombre:['', Validators.required],
-        contraseña:['', Validators.required]
+        nombreUsuario: ['', Validators.required],
+        contraseña: ['', Validators.required]
 
-    }
+      }
     )
   }
 
   ngOnInit(): void {
   }
 
-  get Nombre(){return this.form.get('nombre')}
-  get Contraseña(){return this.form.get('contraseña')}
+  get Nombre() { return this.form.get('nombre') }
+  get Contraseña() { return this.form.get('contraseña') }
+
+  /*Login() {
+    this.authService.login(this.nombreUsuario, this.contraseña);
+  }*/
+
+
+
+
+  
+
+
 
 }
