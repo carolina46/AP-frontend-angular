8f4f1e0bb2337918147ae69b426d4d8ed70fc5eb
Carolina perez <mariacarola46@hotmail.com>
2022-06-26 02:53:02 -0300

    Se agrego los enlaces para editar, eliminar y agregar elementos en el componente habilidades

--- src/app/componentes/habilidades/habilidades.component.ts
+++ src/app/componentes/habilidades/habilidades.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Habilidad } from 'src/app/modelo/habilidad';
 
 @Component({
   selector: 'app-habilidades',
@@ -7,7 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HabilidadesComponent implements OnInit {
 
-  constructor() { }
+  esEdicion: boolean = false;
+
+  habilidades: Habilidad[] = [
+    {
+      id: 1,
+      nombre: "html",
+      porcentajeDominio: 25,
+      imagen: "./assets/html.ico",
+    },
+    {
+      id: 2,
+      nombre: "css",
+      porcentajeDominio: 50,
+      imagen: "./assets/css.ico",
+    },
+    {
+      id: 3,
+      nombre: "java",
+      porcentajeDominio: 75,
+      imagen: "./assets/java.ico",
+    },
+    {
+      id: 4,
+      nombre: "git",
+      porcentajeDominio: 100,
+      imagen: "./assets/git.ico",
+    },
+    {
+      id: 5,
+      nombre: "angular",
+      porcentajeDominio: 25,
+      imagen: "./assets/angular.ico",
+    },
+    {
+      id: 6,
+      nombre: "python",
+      porcentajeDominio: 50,
+      imagen: "./assets/python.ico",
+    }
+
+  ]
+
+
+  constructor(private router: Router) {
+    if (this.router.url === '/edicionPortfolio')
+      this.esEdicion = true;
+  }
 
   ngOnInit(): void {
   }
