78801a26be3fd7eabd91ccb092ef709d3a4d9b0e
Carolina perez <mariacarola46@hotmail.com>
2022-08-07 19:30:33 -0300

    Modifico el componente de educacion para que muestre los datos del backend

--- src/app/componentes/mostrarPortfolio/educacion/educacion.component.ts
+++ src/app/componentes/mostrarPortfolio/educacion/educacion.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { Educacion } from 'src/app/modelo/educacion';
+import { EducacionService } from 'src/app/servicios/educacion.service';
 
 @Component({
   selector: 'app-educacion',
@@ -11,40 +11,26 @@ export class EducacionComponent implements OnInit {
 
   esEdicion: boolean = false;
 
-  educacion: Educacion[] = [
-    {
-      id: 1,
-      nombreInstitucion: "Nombre Instituto",
-      tituloObtenido: "Carrera o titulo obtenido",
-      logoInstitucional: "./assets/educacion.png",
-      desde: "03/2018",
-      hasta: "Actualidad",
-    },
-    {
-      id: 1,
-      nombreInstitucion: "Nombre Instituto",
-      tituloObtenido: "Carrera o titulo obtenido",
-      logoInstitucional: "./assets/educacion.png",
-      desde: "03/2018",
-      hasta: "05/2021",
-    },
-    {
-      id: 1,
-      nombreInstitucion: "Nombre Instituto",
-      tituloObtenido: "Carrera o titulo obtenido",
-      logoInstitucional: "./assets/educacion.png",
-      desde: "03/2018",
-      hasta: "Actualidad",
-    }
-  ]
+  educacion: Educacion[] = [];
 
-
-  constructor(private router: Router) {
-    if (this.router.url === '/edicionPortfolio')
-      this.esEdicion = true;
+  constructor(private educacioService : EducacionService) {
   }
 
   ngOnInit(): void {
-  }
+    this.educacioService.listarEducacions().subscribe(datos => {
+    //ordeno por posicion
+    datos.sort(function (a, b) {
+      if (a.posicion > b.posicion) {
+        return 1;
+      }
+      if (a.posicion < b.posicion) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+    this.educacion = datos;
+  });
+}
 
 }
