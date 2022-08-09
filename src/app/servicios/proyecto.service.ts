5226413de7e25c7ceec5bcce0d6a58cd8b0275eb
Carolina perez <mariacarola46@hotmail.com>
2022-08-09 13:41:48 -0300

    Agrego el servicio para obtener los datos de los proyectos desde el backend

--- src/app/servicios/proyecto.service.ts
+++ src/app/servicios/proyecto.service.ts
@@ -1,9 +1,50 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { Proyecto } from '../modelo/proyecto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProyectoService {
 
-  constructor() { }
+  private url: string = 'http://localhost:8080/proyecto/';
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+
+  constructor(private http: HttpClient) { }
+
+
+  /** GET: Listado de proyectos*/
+  listarProyectos(): Observable<Proyecto[]> {
+    return this.http.get<Proyecto[]>(this.url + 'listar').pipe(
+      catchError(this.handleError<Proyecto[]>('listarProyectos', []))
+    );
+  }
+
+  /** POST: Guardo un proyecto"*/
+  guaradarProyecto(proyecto: Proyecto): Observable<Proyecto> {
+    return this.http.post<Proyecto>(this.url + 'agregar', proyecto, this.httpOptions).pipe(
+      catchError(this.handleError<Proyecto>('guaradarProyecto'))
+    );
+  }
+
+  /** DELETE: Elimino un proyecto */
+  eliminaProyecto(proyecto: Proyecto) {
+    const url = `${this.url + "eliminar"}/${proyecto.id}`;
+    return this.http.delete<Boolean>(url, this.httpOptions).pipe(
+      catchError(this.handleError<Boolean>('guaradarProyecto', false))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(`${operation} Fallo: ${error.message}`);
+      return of(result as T);
+    };
+
+
+  }
 }
